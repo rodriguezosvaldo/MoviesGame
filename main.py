4d4548488c8e614a5fbee03747c0d41fa42ca3a7
Osvaldo <osvaldo.rguez911001@gmail.com>
2024-04-12 13:58:51 -0400

    last before change structure of the code

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ root.geometry("350x350")
 menu = Menu(root)
 root.config(menu=menu)
 
+image_background = Image.open("pictures/Background.jpg")
+w, h = image_background.size
+while w > 350 or h > 350:
+    image_background = image_background.resize((int(w/1.01), int(h/1.01)))
+photo = ImageTk.PhotoImage(image_background)
+label_background = Label(root, image=photo)
+label_background.pack(fill=BOTH, expand=1)
+
 
 
 def animation():
@@ -16,9 +24,80 @@ def animation():
     if "frame_anim" in globals():
         frame_anim.pack_forget()
     
+    
     frame_anim = Frame(root)
     frame_anim.pack(fill=BOTH, expand=1)
     
+    frame_anim.grid_rowconfigure(0, weight=1)
+    frame_anim.grid_rowconfigure(1, weight=2)
+    frame_anim.grid_rowconfigure(2, weight=1)
+    frame_anim.grid_rowconfigure(3, weight=1)
+    frame_anim.grid_rowconfigure(4, weight=1)
+    frame_anim.grid_columnconfigure(0, weight=1)
+    frame_anim.grid_columnconfigure(1, weight=1)
+
+    label_cat = Label(frame_anim, text="Animation")
+    label_cat.grid(row=0, column=0, padx=5, pady=5, sticky="nsew", columnspan=2)
+
+    
+
+    #Declarations
+    
+    list_result = []    
+    list_control = []
+    global img_list
+    img_list = [] 
+
+    img0 = Image.open("pictures/animation/Brave.jpg")
+    img1 = Image.open("pictures/animation/Charm.jpg")
+    img2 = Image.open("pictures/animation/Coco.jpg")
+    img3 = Image.open("pictures/animation/Inside Out.jpg")
+    img4 = Image.open("pictures/animation/Luca.jpg")
+    img5 = Image.open("pictures/animation/Soul.jpg")
+    img6 = Image.open("pictures/animation/The Brave Little Toaster.jpg")
+    img7 = Image.open("pictures/animation/Treasure Planet.jpg")
+    img8 = Image.open("pictures/animation/United.jpg")
+    img9 = Image.open("pictures/animation/Zootopia.jpg")
+    
+    global img_dict
+    img_dict = {"Brave": img0,
+                "Charm": img1, 
+                "Coco": img2, 
+                "Inside Out": img3, 
+                "Luca": img4, 
+                "Soul": img5, 
+                "The Brave Little Toaster": img6, 
+                "Treasure Planet": img7, 
+                "United": img8, 
+                "Zootopia": img9
+                }
+    
+    
+
+    def green_red():
+        frame_right_wrong = Frame(frame_anim)
+        frame_right_wrong.grid(row=2, column=1, padx=30, pady=1, sticky="e", rowspan=2)
+        global list_right_wrong_frames
+        list_right_wrong_frames = []
+        right_wrong_bg = "white"
+    
+        for frame in range(len(img_dict)):
+            row = 0 if frame < 5 else 1
+            column = frame if frame < 5 else frame - 5
+            frame = Frame(frame_right_wrong, bg=right_wrong_bg, borderwidth=1, relief="solid")
+            frame.grid(row=row, column=column, padx=1, pady=1, sticky="nsew")
+            frame.config(width=10, height=10)
+            list_right_wrong_frames.append(frame)
+    green_red()
+
+    def lose_win(text):
+        frame_lose_win = Frame(frame_anim, bg="white")
+        frame_lose_win.pack(fill=BOTH, expand=1)
+        label_result = Label(frame_lose_win, text=text)
+        label_result.pack(pady=5, anchor="center")
+        label_percent = Label(frame_lose_win, text=f"{percentage}%", bg="green" if percentage >= 50 else "red")
+        label_percent.pack(pady=5, anchor="center") 
+
     def create_rb_text():
         global rb1, rb2, rb3
         if "rb1" in globals():
@@ -57,32 +136,51 @@ def animation():
         rb2.grid(row=3, column=0, padx=60, pady=1, columnspan=2, sticky="w")
         rb3 = Radiobutton(frame_anim, text=(rb_text[2]), value=(rb_text[2]), variable=selected_movie)
         rb3.grid(row=4, column=0, padx=60, pady=1, columnspan=2, sticky="w")
-        
-    list_control = []   
+
+    
+      
     def confirm():
+        
         if selected_movie.get() == "0":
             return
         
-        
+        if len(list_control) == len(img_dict):
+            print(list_result)
+
         if selected_movie.get() == key_list[value_list.index(img)]:
+            result = 1
             control = 0
             while control in list_control:
                 control += 1
             list_right_wrong_frames[control].config(bg="green")
             list_control.append(control)
+            list_result.append(result)
         else:
+            result = 0
             control = 0
             while control in list_control:
                 control += 1
             list_right_wrong_frames[control].config(bg="red")
             list_control.append(control)
+            list_result.append(result)
+            
+        if len(list_control) < len(img_dict):
+            show_image()
+        else:
+            global percentage
+            percentage = int(sum(list_result)*100/(len(list_result)))
+            for widgets in frame_anim.winfo_children():
+                widgets.destroy()
+            
+            if percentage >= 50:  
+                lose_win("CONGRATULATIONS")
+            else:
+                lose_win("NEXT TIME")
 
-            
-                    
-            
-        
-        show_image()
-        
+
+
+
+
     def show_image():
         global img
         img = random.choice(list(img_dict.values()))
@@ -101,65 +199,9 @@ def animation():
         create_rb_text()
 
     
-    
-    img0 = Image.open("pictures/animation/Brave.jpg")
-    img1 = Image.open("pictures/animation/Charm.jpg")
-    img2 = Image.open("pictures/animation/Coconut.jpg")
-    img3 = Image.open("pictures/animation/Inside Out.jpg")
-    img4 = Image.open("pictures/animation/Luca.jpg")
-    img5 = Image.open("pictures/animation/Soul.jpg")
-    img6 = Image.open("pictures/animation/The Brave Little Toaster.jpg")
-    img7 = Image.open("pictures/animation/Treasure Planet.jpg")
-    img8 = Image.open("pictures/animation/United.jpg")
-    img9 = Image.open("pictures/animation/Zootopia.jpg")
-    
-    global img_dict
-    img_dict = {"Brave": img0,
-                "Charm": img1, 
-                "Coconut": img2, 
-                "Inside Out": img3, 
-                "Luca": img4, 
-                "Soul": img5, 
-                "The Brave Little Toaster": img6, 
-                "Treasure Planet": img7, 
-                "United": img8, 
-                "Zootopia": img9
-                }
-    
-    global img_list
-    img_list = []
     show_image()
-
-
-    frame_anim.grid_rowconfigure(0, weight=1)
-    frame_anim.grid_rowconfigure(1, weight=2)
-    frame_anim.grid_rowconfigure(2, weight=1)
-    frame_anim.grid_rowconfigure(3, weight=1)
-    frame_anim.grid_rowconfigure(4, weight=1)
-    frame_anim.grid_columnconfigure(0, weight=1)
-    frame_anim.grid_columnconfigure(1, weight=1)
-
-
-
-    label_cat = Label(frame_anim, text="Animation")
-    label_cat.grid(row=0, column=0, padx=5, pady=5, sticky="nsew", columnspan=2)
-
     
     
-    
-    global frame_right_wrong
-    frame_right_wrong = Frame(frame_anim)
-    frame_right_wrong.grid(row=2, column=1, padx=30, pady=1, sticky="e", rowspan=2)
-    list_right_wrong_frames = []
-    right_wrong_bg = "white"
-    
-    for frame in range(len(img_dict)):
-        row = 0 if frame < 5 else 1
-        column = frame if frame < 5 else frame - 5
-        frame = Frame(frame_right_wrong, bg=right_wrong_bg, borderwidth=1, relief="solid")
-        frame.grid(row=row, column=column, padx=1, pady=1, sticky="nsew")
-        frame.config(width=10, height=10)
-        list_right_wrong_frames.append(frame)
 
     btn_confirm = Button(frame_anim, text="Confirm", command=confirm)
     btn_confirm.grid(row=4, column=1, padx=30, pady=1, sticky="ne")
